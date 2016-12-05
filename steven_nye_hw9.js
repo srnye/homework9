0b7eebea48df81c1704ea7cf074a2daf7f36445a
Steven R. Nye <stevennye@mail.weber.edu>
2016-12-04 23:33:36 -0700

    added code to create customer directory

--- steven_nye_hw9.js
+++ steven_nye_hw9.js
@@ -34,28 +34,51 @@ if (process.argv.length <= 2)
 	usageState();
 	process.exit(-1);
 }
-else if (process.argv.length > 8)
+else if (process.argv.length != 8)
 {
-	usageState();
+	console.log("Incorrect number of arguments, exiting...");
 	process.exit(-1);
 }
 else if (process.argv[2] != "-c")
 {
-	usageState();
+	console.log("Invalid argument, exiting...");
 	process.exit(-1);
 }
 else if (process.argv[4] != "-f")
 {
-	usageState();
+	console.log("Invalid argument, exiting...");
 	process.exit(-1);
 }
 else if (process.argv[6] != "-u")
 {
-	usageState();
+	console.log("Invalid argument, exiting...");
 	process.exit(-1);
 }
 
+//Save args
+var dataFolder = process.argv[3];
+var dataFile = process.argv[5];
+var userName = process.argv[7];
 
-console.log(process.argv[2]);
-console.log(process.argv[3]);
-console.log(process.argv[4]);
+var fs = require('fs');
+
+console.log("Working on " + process.env.HOME);
+console.log("Checking for data structure...");
+
+//check if folder structure exists
+if (!fs.existsSync(dataFolder))
+{
+	//if folder structure doesnt exist, create it
+	console.log("Customer " + dataFolder + " folder is missing");
+	console.log("Creating folder...");
+	fs.mkdirSync(dataFolder);
+	var months = ['01','02','03','04','05','06','07','08','09','10','11','12'];
+	for (var i in months)
+	{
+		fs.mkdirSync(dataFolder+"/"+months[i]);
+	}
+}
+
+console.log(dataFolder);
+console.log(dataFile);
+console.log(userName);
