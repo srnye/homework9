b5dbb8d90bb00cc9d70a86fab1fde8f1c11bae61
Steven R. Nye <stevennye@mail.weber.edu>
2016-12-05 01:41:54 -0700

    added scp modules, fixed README, added more code to js file

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ Final project - Javascript
 # Steps to download node.js:
 1. In the terminal, type 'sudo apt-get install -y nodejs'
 2. Next, type 'sudo apt install nodejs-legacy'
+
+# This project will also include using SCP2, to download SCP2 (make sure youre at ~):
+1. In the terminal, type 'sudo apt install npm'
+2. After npm is installed, type 'sudo npm install scp2 -g'
--- steven_nye_hw9.js
+++ steven_nye_hw9.js
@@ -13,7 +13,7 @@
 //Declaration of usage function
 function usageState()
 {
-	console.log("Usage: node steven_nye_hw9.js [-c customerDataFolder] [-f dataFile] [-u userName]");
+	console.log("Usage: node steven_nye_hw9.js [-c customerDataFolder] [-f dataFile] [-u userName] [-p userPass]");
 	console.log("All arguments are REQUIRED");
 }
 
@@ -25,6 +25,7 @@ function helpFun()
 	console.log("-c Creates customer folder on the home directory (if there isn't one already)");
 	console.log("-f Specifies which datafile will be copied over from the directory");
 	console.log("-u Specifies user's username on the customer directory (icarus server)");
+	console.log("-p Specifies user's password on the customer directory (icarus server)");
 	console.log("With no arguments, it provides a usage statement");
 }
 
@@ -34,7 +35,7 @@ if (process.argv.length <= 2)
 	usageState();
 	process.exit(-1);
 }
-else if (process.argv.length != 8)
+else if (process.argv.length != 10)
 {
 	console.log("Incorrect number of arguments, exiting...");
 	process.exit(-1);
@@ -54,11 +55,17 @@ else if (process.argv[6] != "-u")
 	console.log("Invalid argument, exiting...");
 	process.exit(-1);
 }
+else if (process.argv[8] != "-p")
+{
+	console.log("Invalid argument, exiting...");
+	process.exit(-1);
+}
 
 //Save args
 var dataFolder = process.argv[3];
 var dataFile = process.argv[5];
 var userName = process.argv[7];
+var passWrd = process.argv[9];
 
 var fs = require('fs');
 
@@ -78,7 +85,24 @@ if (!fs.existsSync(dataFolder))
 		fs.mkdirSync(dataFolder+"/"+months[i]);
 	}
 }
+else
+{
+	console.log("Customer " + dataFolder + " folder is already created.");
+}
 
-console.log(dataFolder);
-console.log(dataFile);
-console.log(userName);
+//get date info
+var todayDate = new Date().toISOString().slice(0,10);
+var d = new Date();
+var curMonth = todayDate.split("-")[1];
+
+console.log("Getting file from customer server");
+
+//connect to customer server and retrieve customer file
+var client = require('scp2');
+client.scp({
+	host: 'icarus.cs.weber.edu',
+	username: userName,
+	password: passWrd,
+	path: '/home/hvalle/submit/cs3030/files/'+dataFile
+	}, dataFolder+'/'+curMonth+'/'+dataFile+'.'+todayDate, function(err) {if (err) {console.log("SCP failed, check credentials");} else{console.log("SCP successful, file located at [" + dataFolder+'/'+curMonth+'/'+dataFile+'.'+todayDate+']');}});
+
