1cd66c9212710bb349477b59b726500311a29b6e
Steven R. Nye <stevennye@mail.weber.edu>
2016-12-04 22:43:54 -0700

    added help function

--- steven_nye_hw9.js
+++ steven_nye_hw9.js
@@ -16,5 +16,14 @@ function usageState()
 	console.log("Usage: node steven_nye_hw9.js [-c customerDataFolder] [-f dataFile] [-u userName]");
 	console.log("All arguments are REQUIRED");
 }
-//Declaration of help function
 
+//Declaration of help function
+function helpFun()
+{
+	console.log("Usage: node steven_nye_hw9.js --help");
+	console.log("--help Prints this help message");
+	console.log("-c Creates customer folder on the home directory (if there isn't one already)");
+	console.log("-f Specifies which datafile will be copied over from the directory");
+	console.log("-u Specifies user's username on the customer directory (icarus server)");
+	console.log("With no arguments, it provides a usage statement");
+}
